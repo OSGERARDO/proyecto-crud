44c151a033e1327bfd26bfb003b8059aa160ecf5
OSGERARDO <nmsosx@gmail.com>
2023-06-09 23:38:52 -0400

    actualizacion codigo javascript

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function validarFormulario(e) {
         return;
     }
     if(editando) {
+        //Editar Producto
         editarProducto();
         editando = false;
     } else {
@@ -35,5 +36,122 @@ function validarFormulario(e) {
 }
 
 }
+
+function agregarProducto() {
+    listaProductos.push({...objProducto});
+
+    mostrarProductos();
+
+    formulario.reset(); //limpiar los input
+
+    limpiarObjeto();
+
+}
+
+function limpiarObjeto() {  //limpiar objeto
+    objProducto.id = '';
+    objProducto.nombre = '';
+    objProducto,cantidad = '';
+}
+
+
+
+
+
+function mostrarProductos() {
+
+    limpiarHTML();
+
+    const divProductos = document.querySelector('.div-productos')
+
+    listaProductos.forEach( producto => {
+        const {id, nombre, cantidad} = producto;
+
+        const parrafo = document.createElement('p');
+        parrafo.textContent = ` ${id} - ${nombre} - ${cantidad} - `;
+        parrafo.dataset.id = id;
+
+        const editarBoton = document.createElement('button');
+        editarBoton.onclik = () => cargarProducto(producto);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-editar');
+        parrafo.append(editarBoton);
+
+
+        const eliminarBoton = document.createElement('button');
+        //eliminarBoton.onclik = () => eliminarProducto(id);//
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-eliminar');
+        parrafo.append(eliminarBoton);
+
+        const hr = document.createElement('hr');
+
+        divProductos.appendChild(parrafo);
+        divProductos.appendChild(hr);
+
+
+
+
+    });
+
+}
+
+function cargarProducto(producto) {
+    const {id, nombre, cantidad} = producto;
+
+    nombreInput.value = nombre;
+    cantidadInput.value = cantidad;
+
+    objProducto.id = id;
+
+    formulario.querySelector('button[type="submit"]').textContent ='Actualizar';
+
+    editando = true;
+}
+
+function editarProducto () {
+    objProducto.nombre = nombreInput.value;
+    objProducto.cantidad = cantidadInput.value;
+
+    listaProductos.map( producto => {
+
+        if(producto.id === objProducto.id) {
+            producto.id = objProducto.id;
+            producto.nombre = objProducto.nombre;
+            producto.cantidad = objProducto.cantidad;
+        }
+
+
+
+
+
+
+    })
+limpiarHTML();
+mostrarProductos();
+
+formulario.reset();
+
+formulario.querySelector('button[type="submit]').textContent = 'Agregar'
+
+editando = false;
+
+}
+
+
+
+
+
+
+function limpiarHTML() {
+    const divProductos = document.querySelector('.div-productos')
+    while(divProductos.firstChild) {
+           divProductos.removeChild(divProductos.firstChild);
+    }
+
+
+}
+
+
 
 
